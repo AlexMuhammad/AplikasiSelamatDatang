ac83335ef60d9ab590f7394a1bd56a9dae9bb0a0
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2021-10-13 16:00:52 +0800

    Add files via upload

---
+++ AplikasiSelamatDatang/AplikasiSelamatDatang/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikasiSelamatDatang
+{
+    public partial class frmSelamatDatang : Form
+    {
+        public frmSelamatDatang()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKeterangan2_Click(object sender, EventArgs e)
+        {
+            lblKeterangan.Text = "Begitu mudahnya membuat aplikasi berbasis windows";
+        }
+
+        private void btnKeterangan1_Click(object sender, EventArgs e)
+        {
+            lblKeterangan.Text = "Selamat Datang Di Pemrograman C#";
+        }
+
+        private void lblKeterangan_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
